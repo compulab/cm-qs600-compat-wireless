5ce8e1586d5483f9fa53104bf18d857b07cd7be8
Pavan Kumar <pavan@codeaurora.org>
2013-01-11 11:18:14 -0800

    ath6kl: AR6004 3.0 support.
    This patch adds support for AR6004 v3.0 hardware.
    
    Change-Id: Iaac924efeb112532c6f859f17791d63b4e0d8ce9

--- drivers/net/wireless/ath/ath6kl/core.h
+++ drivers/net/wireless/ath/ath6kl/core.h
@@ -234,6 +234,16 @@ enum ath6kl_hw_flags {
 #define AR6004_HW_1_6_DEFAULT_BOARD_DATA_FILE \
 	"ath6k/AR6004/hw1.6/bdata.bin"
 
+/* AR6004 3.0 definitions */
+#define AR6004_HW_3_0_VERSION			0x31C809F8
+#define AR6004_HW_3_0_FW_DIR			"ath6k/AR6004/hw3.0"
+#define AR6004_HW_3_0_FIRMWARE_FILE		"fw.ram.bin"
+#define AR6004_HW_3_0_TCMD_FIRMWARE_FILE	"utf.bin"
+#define AR6004_HW_3_0_UTF_FIRMWARE_FILE		"utf.bin"
+#define AR6004_HW_3_0_BOARD_DATA_FILE		"ath6k/AR6004/hw3.0/bdata.bin"
+#define AR6004_HW_3_0_DEFAULT_BOARD_DATA_FILE  	"ath6k/AR6004/hw3.0/bdata.bin"
+#define AR6004_HW_3_0_EPPING_FILE		"epping.bin"
+
 /* Per STA data, used in AP mode */
 #define STA_PS_AWAKE		BIT(0)
 #define	STA_PS_SLEEP		BIT(1)
